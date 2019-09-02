a823ba3a7bf7db596dcb5a70de64838f5b47aa59
Kheirallah Ashkar <kheirallah.j.ashkar@tsgforce.com>
2019-09-02 13:56:31 -0500

    adding valid parenthesis problem

---
+++ src/com/kheirallah/inc/strings/ValidParenthesis.java
@@ -0,0 +1,62 @@
+package com.kheirallah.inc.strings;
+
+//Easy
+
+/*
+Given a string containing just the characters ‘(‘, ‘)’, ‘{‘, ‘}’, ‘[‘, and ‘]’, determine if the input string is valid.
+
+An input string is valid if:
+1. Open brackets must be closed by the same type of brackets
+2. Open brackets must be closed in the correct order
+
+Note that an empty string is also considered valid.
+
+Example 1: Input: “()”
+Output: true
+
+Example 2: Input: “()[]{}”
+Output: true
+
+Example 3: Input: “(]”
+Output: false
+
+Example 4: Input: “([)]”
+Output: false
+
+Example 5: Input: “{[]}”
+Output: true
+ */
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+    public static void main(String[] args) {
+        String input = "{()}";
+
+        System.out.println(isValidParenthesis(input));
+    }
+
+    //Time Complexity O(n)
+    //Space Complexity O(n), for new stack
+    private static boolean isValidParenthesis(String input) {
+        if (input.isEmpty()) {
+            return true;
+        }
+
+        Stack<Character> s = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            if (c == '{') {
+                s.push('}');
+            } else if (c == '(') {
+                s.push(')');
+            } else if (c == '[') {
+                s.push(']');
+            } else if (s.isEmpty() || s.pop() != c) {
+                return false;
+            }
+        }
+        return s.isEmpty();
+    }
+}
