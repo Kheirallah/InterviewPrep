30ae2d4e40165cd27654652b1f7af599544256ce
Kheirallah Ashkar <kheirallah.j.ashkar@tsgforce.com>
2019-10-06 20:51:47 -0500

    StackMin & single array 3 stacks

---
+++ src/com/kheirallah/inc/stacks/SingleArrayThreeStacks.java
@@ -0,0 +1,68 @@
+package com.kheirallah.inc.stacks;
+
+//Easy
+//Crack the coding interview 3.1
+
+/*
+Describe how you could use a single array to implement 3 stacks
+ */
+
+public class SingleArrayThreeStacks {
+
+    public static void main(String args[]) {
+
+    }
+
+    class FixedMultiStack {
+        private int numberOfStacks = 3;
+        private int stackCapacity;
+        private int[] values;
+        private int[] sizes;
+
+        public FixedMultiStack(int stackSize) {
+            this.stackCapacity = stackSize;
+            values = new int[stackSize * numberOfStacks];
+            sizes = new int[numberOfStacks];
+        }
+
+        public void push(int stackNum, int value) throws Exception {
+            if (isFull(stackNum)) {
+                throw new Exception("Cannot add to full stack");
+            }
+            sizes[stackNum]++;
+            values[indexOfTop(stackNum)] = value;
+        }
+
+        public int pop(int stackNum) throws Exception {
+            if (isEmpty(stackNum)) {
+                throw new Exception("Cannot pop from empty stack");
+            }
+            int topIndex = indexOfTop(stackNum);
+            int value = values[topIndex];
+            values[topIndex] = 0;
+            sizes[stackNum]--;
+            return value;
+        }
+
+        public int peek(int stackNum) throws Exception {
+            if (isEmpty(stackNum)) {
+                throw new Exception("Cannot peek into empty stack");
+            }
+            return values[indexOfTop(stackNum)];
+        }
+
+        public boolean isEmpty(int stackNum) {
+            return sizes[stackNum] == 0;
+        }
+
+        public boolean isFull(int stackNum) {
+            return sizes[stackNum] == stackCapacity;
+        }
+
+        private int indexOfTop(int stackNum) {
+            int offset = stackNum * stackCapacity;
+            int size = sizes[stackNum];
+            return offset + size - 1;
+        }
+    }
+}
---
+++ src/com/kheirallah/inc/stacks/StackMin.java
@@ -0,0 +1,50 @@
+package com.kheirallah.inc.stacks;
+
+//Easy
+//Crack the coding interview 3.2
+
+/*
+How would you design a stack which in addition to push and pop, has a function min which returns the minimum element? Push, pop, and min should all operate in O(1) time.
+
+Example:
+    push(5); stack is {5}, min is 5
+    push(6); stack is {6, 5}, min is 5
+    push(3); stack is {3, 6, 5}, min is 3
+    push(7); stack is {7, 3, 6, 5}, min is 3
+    pop(); pops 7. stack is {3, 6, 5}, min is  3
+    pop(); pops 3. stack is {6, 5}, min is  5
+ */
+
+import java.util.Stack;
+
+public class StackMin extends Stack<StackMin.NodeWithMin> {
+
+    public static void main(String args[]) {
+
+    }
+
+    public void push(int value) {
+        int newMin = Math.min(value, min());
+        super.push(new NodeWithMin(value, newMin));
+
+    }
+
+    public int min() {
+        if (this.isEmpty()) {
+            return Integer.MIN_VALUE; //error
+        } else {
+            NodeWithMin n = peek();
+            return n.min;
+        }
+    }
+
+    class NodeWithMin {
+        public int value;
+        public int min;
+
+        public NodeWithMin(int value, int min) {
+            this.value = value;
+            this.min = min;
+        }
+    }
+}
